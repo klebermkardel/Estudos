e006ba31adac4d4f01f2e1a5c80dcb115ca8ab26
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-16 20:48:26 -0300

    Cria função para verificar se lâmpada está quebrada e caso esteja não faz nenhuma outra ação de ligar e desligar

--- Praticando JavaScript/Projetos/Lâmpada/lampada.js
+++ Praticando JavaScript/Projetos/Lâmpada/lampada.js
@@ -2,12 +2,20 @@ const turnOn = document.getElementById('turnOn')
 const turnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
 
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
 function lampOn() {
-    lamp.src = './img/ligada.jpg'
+    if(!isLampBroken()) {
+        lamp.src = './img/ligada.jpg'
+    }
 }
 
 function lampOff() {
-    lamp.src = './img/desligada.jpg'
+    if(!isLampBroken()) {
+        lamp.src = './img/desligada.jpg'
+    }
 }
 
 function lampBroken() {
