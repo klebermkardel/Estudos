b265e27656b43a626eae707e4d949e9956bbdd27
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 14:10:38 -0300

    Cria função que troca cor do semáforo a cada 3 segundos ao clicar no botão automático

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -1,15 +1,22 @@
 const img = document.getElementById('img')
 const buttons = document.getElementById('buttons')
+let colorIndex = 0
 
 const trafficLight = (event) => {
     turnOn[event.target.id]()
 }
 
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green']
+    const color = colors[colorIndex]
+    turnOn[color]()
+} 
+
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => setInterval(changecolor, 1000)
+    'automatic': () => setInterval(changeColor, 3000)
 }
 
 buttons.addEventListener('click', trafficLight)
