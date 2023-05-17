5f6660e8da33e1dcc1d944a9e772a1a1a3759268
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 14:33:05 -0300

    Cria nova função para pausar o a ação do botão automático quando clicarmos em outro botão

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -1,8 +1,10 @@
 const img = document.getElementById('img')
 const buttons = document.getElementById('buttons')
 let colorIndex = 0
+let intervalId = null
 
 const trafficLight = (event) => {
+    stopAutomatic()
     turnOn[event.target.id]()
 }
 
@@ -14,13 +16,17 @@ const changeColor = () => {
     const color = colors[colorIndex]
     turnOn[color]()
     nextIndex()
-} 
+}
+
+const stopAutomatic = () => {
+    clearInterval(intervalId)
+}
 
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => setInterval(changeColor, 1000)
+    'automatic': () => intervalId = setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener('click', trafficLight)
