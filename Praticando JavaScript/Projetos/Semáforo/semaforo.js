59349be2f117c3e44bdd274de018f15b7869080d
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 14:02:38 -0300

    Cria nova função dentro do objeto literal que vai chamar função que executa ação do click no botão automático

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -8,7 +8,8 @@ const trafficLight = (event) => {
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
-    'green': () => img.src = './img/verde.png'
+    'green': () => img.src = './img/verde.png',
+    'automatic': () => setInterval(changecolor, 1000)
 }
 
 buttons.addEventListener('click', trafficLight)
