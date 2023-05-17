b1da809476e22882c23a0e75032d77860ee2905e
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 13:56:28 -0300

    Chama método do objeto e filtra qual ação está sendo feito através do alvo do evento usando o id

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -2,7 +2,7 @@ const img = document.getElementById('img')
 const buttons = document.getElementById('buttons')
 
 const trafficLight = (event) => {
-    
+    turnOn[event.target.id]()
 }
 
 const turnOn = {
