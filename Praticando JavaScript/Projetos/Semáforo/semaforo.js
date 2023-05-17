04bd670fa861567abd20063a272bc2f44ba490f3
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 14:23:20 -0300

    Cria função nextIndex para que seja alterada para cada cor correspondente a cada segundo

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -6,17 +6,21 @@ const trafficLight = (event) => {
     turnOn[event.target.id]()
 }
 
+const nextIndex= () => colorIndex = colorIndex < 2 ? ++colorIndex : 0
+
+
 const changeColor = () => {
     const colors = ['red', 'yellow', 'green']
     const color = colors[colorIndex]
     turnOn[color]()
+    nextIndex()
 } 
 
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => setInterval(changeColor, 3000)
+    'automatic': () => setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener('click', trafficLight)
