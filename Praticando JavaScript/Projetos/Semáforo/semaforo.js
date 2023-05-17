3487c18f184f03d63dd7bbfcac44dde3fc126da6
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-17 13:52:32 -0300

    Cria funções referente a ação de cada botão dentro de um objeto literal

--- Praticando JavaScript/Projetos/Semáforo/semaforo.js
+++ Praticando JavaScript/Projetos/Semáforo/semaforo.js
@@ -5,4 +5,10 @@ const trafficLight = (event) => {
     
 }
 
+const turnOn = {
+    'red': () => img.src = './img/vermelho.png',
+    'yellow': () => img.src = './img/amarelo.png',
+    'green': () => img.src = './img/verde.png'
+}
+
 buttons.addEventListener('click', trafficLight)
