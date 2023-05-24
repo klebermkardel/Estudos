4583502777b629e5f110038988dfadd4d5761d7f
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-24 12:58:34 -0300

    Introdução sobre componentes e props no React

--- Cursos/Santander Coders - Front End/React/meu-projeto-react/src/App.jsx
+++ Cursos/Santander Coders - Front End/React/meu-projeto-react/src/App.jsx
@@ -1,8 +1,22 @@
+import MeuComponente from "./components/MeuComponente"
 
 function App() {
-
   return (
-    <h1>Olá Mundo, React!</h1>
+    <div>
+      <h1>Olá Mundo, React!</h1>
+      <MeuComponente />
+      <MeuComponente />
+      <MeuBotao conteudo='me clique' />
+
+      <MeuBotao conteudo='depois aqui' />
+      <MeuBotao conteudo='e por fim aqui' idade={2} />
+    </div>
+  )
+}
+
+function MeuBotao(props) {
+  return (
+    <button>{props.conteudo}</button>
   )
 }
 
---
+++ Cursos/Santander Coders - Front End/React/meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente() {
+    return (
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sed magnam commodi facere obcaecati autem, deserunt unde cum nostrum id eos repellat labore odio, fugit facilis, molestias dignissimos beatae blanditiis!</p>
+    )
+}
