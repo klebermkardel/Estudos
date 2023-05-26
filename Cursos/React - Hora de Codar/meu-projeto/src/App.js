407d28db8adbcae3166aa6d6c40f3271d0d1b207
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-26 17:50:19 -0300

    Mostra como funciona a criação de tags dentro do JSX

--- Cursos/React - Hora de Codar/meu-projeto/src/App.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Olá, React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
     </div>
   );
 }
