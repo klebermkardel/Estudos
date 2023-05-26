d5d2b6b116339a095a5286a3e55657c066349a92
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-26 18:21:00 -0300

    Cria e importa primeiro componente chamado HelloWorld

--- Cursos/React - Hora de Codar/meu-projeto/src/App.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Kleber'
@@ -16,6 +17,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <img src={url} alt="Minha Imagem" />
+      <HelloWorld />
     </div>
   );
 }
