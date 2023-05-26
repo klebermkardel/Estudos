11e1479120c1e138d2d7cce40a22d1c92008a8b2
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-26 17:58:00 -0300

    Explica como interpolar variáveis e apresentar informações no front

--- Cursos/React - Hora de Codar/meu-projeto/src/App.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Kleber'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
     </div>
   );
 }
