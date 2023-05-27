45c77d5dd9af43258a69568c54d5481fe72ca8ac
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-27 10:26:27 -0300

    Cria componente Pessoa.js para mostrar como trabalhar com várias props no mesmo componente

--- Cursos/React - Hora de Codar/meu-projeto/src/App.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
   const nome = 'Maria'
 
   return (
     <div className="App">
-      <HelloWorld />
       <SayMyName nome="Kleber"/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="Kleber"
+        idade="27"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
