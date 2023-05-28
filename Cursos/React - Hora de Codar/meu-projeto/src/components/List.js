1db2437f5471a512d1066618f632d8525b05e7e0
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-28 09:55:09 -0300

    Mostra como adicionar types para as props, como trabalhar com validações usando types e como usar defaultProps

--- Cursos/React - Hora de Codar/meu-projeto/src/components/List.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/components/List.js
@@ -5,8 +5,12 @@ function List() {
         <>
             <h1>Lista</h1>
             <ul>
-                <Item marca="Samsung" />
-                <Item marca="Motorola"/>
+                <Item marca="Samsung" ano_lancamento={1988}/>
+                <Item marca="Motorola" ano_lancamento={1973}/>
+                <Item marca="Apple" />
+                <Item marca="LG" ano_lancamento={2011} />
+                <Item  />
+
             </ul>
         </>
     )
