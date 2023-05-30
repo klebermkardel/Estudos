5368c75344c43b3687997116accb889a13b9a4e0
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-30 15:24:34 -0300

    Altera arquivo de componente Footer.js para mostrar como importar react icons e como trabalhar com eles

--- Cursos/React - Hora de Codar/meu-projeto/src/components/layout/Footer.js
+++ Cursos/React - Hora de Codar/meu-projeto/src/components/layout/Footer.js
@@ -1,5 +1,16 @@
+import { FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return (
+        <footer>
+            <ul>
+                <li><FaFacebook /></li>
+                <li><FaTwitter /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
+    )
 }
 
 export default Footer
