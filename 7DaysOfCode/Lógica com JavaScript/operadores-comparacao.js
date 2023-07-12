7f5ed5123ba2db82c7626319f8cf37edc4148b62
Kleber Molina Kardel <molinakardel@gmail.com>
2023-07-12 09:48:03 -0300

    Atualiza nome do arquivo

--- 7DaysOfCode/Lógica com JavaScript/operadores-comparacao.js
+++ 7DaysOfCode/Lógica com JavaScript/operadores-comparacao.js
@@ -11,11 +11,16 @@ if(numeroUm == stringUm) {
     console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
+console.log('-------------------------------------')
+
 if(numeroTrinta === stringTrinta) {
     console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
     console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
+
+console.log('-------------------------------------')
+
 
 if (numeroDez == stringDez) {
     console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
