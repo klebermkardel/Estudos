416afc2664dd2161015e310d167ab5d64d3e2844
klebermkardel <molinakardel@gmail.com>
2023-08-01 19:18:11 -0300

    Cria pasta 'Switch' dentro da pasta 'Estruturas Condicionais' com conteúdo sobre a estrutura condicional Switch

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Estruturas Condicionais/Switch/script.js
@@ -0,0 +1,16 @@
+// Estruturas Condicionais (Switch)
+
+// Switch
+
+const fruta = "Maçã";
+
+switch (fruta) {
+    case "Banana":
+        console.log("Banana é a fruta");
+        break;
+    case "Maçã":
+        console.log("Maçã é a fruta");
+        break;
+    default: // Executa script caso nenhum dos cases sejam atendidos
+        console.log("Fruta não encontrada");
+}
