1cd5a1acc8c3c52bfe1f68aead61aabe56474952
klebermkardel <molinakardel@gmail.com>
2023-08-01 12:01:49 -0300

    Cria pasta 'Convertendo Tipos de Dados' com conteúdo ensinando sobre como converter uma string para number

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Convertendo Tipo de Dados/script.js
@@ -0,0 +1,10 @@
+// Fundamentos - Convertendo Tipos de Dados
+
+// Conversão de Tipos
+const meuNumero = "123";
+const meuNumeroConvertido = Number(meuNumero); // converte string para número
+console.log(meuNumero); // retorna "123"
+console.log(typeof meuNumero); // retorna string
+console.log(meuNumeroConvertido); // retorna 123
+console.log(typeof meuNumeroConvertido); // retorna number
+
