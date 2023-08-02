12c267f22bd031dba796feb3c498caf541dae10b
klebermkardel <molinakardel@gmail.com>
2023-08-02 14:08:09 -0300

    Cria pasta 'Truthy-falsy' dentro de 'Fundamentos' com conteúdo explicando o conceito de valores truthy e falsy

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Truthy-falsy/script.js
@@ -0,0 +1,19 @@
+// Fundamentos - Truthy & Falsy
+
+const minhaVariavel1 = ""; // falsy
+const minhaVariavel2 = "Algum texto"; // Truthy
+
+if(minhaVariavel1) {
+    console.log("É verdadeiro!")
+} else {
+    console.log("É falso!")
+}
+
+if(minhaVariavel2) {
+    console.log("É verdadeiro!")
+} else {
+    console.log("É falso!")
+}
+
+
+
