f9f4cb8e889f2fdcbd7f29283d2c38b9c76d9155
klebermkardel <molinakardel@gmail.com>
2023-08-02 10:22:46 -0300

    Cria dentro da pasta 'Fundamentos' pasta 'Estruturas de Repetição' com conteúdo sobre as loops for, while e do-while

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Estruturas de Repetição/For-While-DoWhile/script.js
@@ -0,0 +1,36 @@
+// Fundamentos - Estruturas de Repetição
+
+// Loop For
+console.log("Loop For")
+
+// Estrutura Loop For: contador, condição de limite, incremento
+for(let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("---------------------");
+
+
+
+// Loop While
+console.log("Loop While");
+
+let i = 0;
+
+while(i <= 5) {
+    console.log(i);
+    i++;
+}
+
+console.log("---------------------");
+
+
+// Loop Do While
+console.log("Loop Do While")
+
+let j = 0;
+
+do {
+    console.log(j);
+    j++;
+} while (j <= 5)
