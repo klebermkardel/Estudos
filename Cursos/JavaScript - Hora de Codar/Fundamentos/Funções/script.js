748f0b4d94649806e03455f2a6b433dbf670c5d8
klebermkardel <molinakardel@gmail.com>
2023-08-02 10:50:04 -0300

    Cria pasta 'Funções' dentro de 'Fundamentos' com conteúdo sobre functions

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Funções/script.js
@@ -0,0 +1,17 @@
+// Fundamentos - Funções
+
+// Estrutura: function nome(argumentos) {corpo}
+
+function cumprimentar(nome) {
+    console.log("Olá, " + nome);       
+}
+
+cumprimentar("Kleber");
+
+function contador() {
+    for(i = 0; i < 5; i++) {
+        console.log(i);
+    }
+}
+
+contador();
