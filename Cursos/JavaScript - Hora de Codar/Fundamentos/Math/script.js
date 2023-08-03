e6d9a8029e69be58c9c84c97998300e624881214
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-03 12:36:26 -0300

    Cria dentro da pasta 'Fundamentos' pasta chamada 'Math' com conteúdo sobre a utilização do objeto Math no javascript

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Math/script.js
@@ -0,0 +1,9 @@
+// Fundamentos - Math
+
+console.log(`Valor de PI: ${Math.PI}`); // retorna valor de PI
+
+console.log(`Arredonda número para o inteiro mais próximo ex: (3.6): ${Math.round(3.6)}`); // Retorna 4
+
+console.log(`Raiz quadrada (25): ${Math.sqrt(25)}`); // Retorna 5
+
+console.log(`Número elevado a sua potência (2³): ${Math.pow(2, 3)}`); // Retorna 8
