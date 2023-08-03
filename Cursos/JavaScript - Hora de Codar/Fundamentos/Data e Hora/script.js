437bdcab4f773af79a2f05c383bc8ded769e6f3c
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-03 12:03:55 -0300

    Cria dentro da pasta 'Fundamentos' a pasta 'Dta e Hora' com conteúdo ensinando sobre como trabalhar com datas e horários no javascript

---
+++ Cursos/JavaScript - Hora de Codar/Fundamentos/Data e Hora/script.js
@@ -0,0 +1,8 @@
+// Fundamentos - Data e Hora
+
+// Data e Hora
+const agora = Date();
+console.log(agora); // Exibe data e hora atual
+
+const natal = new Date(2023, 11, 25);
+console.log(natal); // Exibe data no Natal no ano atual
