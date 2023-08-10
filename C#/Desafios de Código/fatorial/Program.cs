90fa4f9d43ac553a88adde94b36c278bff477281
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-10 08:49:02 -0300

    Cria pasta para estudos de C#

---
+++ C#/Desafios de Código/fatorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalculoFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            long fatorial = CalculaFatorial(numero);
+
+            Console.WriteLine("O fatorial de " + numero + " é: " + fatorial);
+        }
+
+        static long CalculaFatorial(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                return 1;
+            }
+
+            long fatorial = 1;
+            for (int i = 2; i <= numero; i++)
+            {
+                fatorial *= i;
+            }
+
+            return fatorial;
+        }
+    }
+}
