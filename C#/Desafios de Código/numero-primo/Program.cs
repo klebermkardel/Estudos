87d1d6edcf235cf1b6240c2134d49bd8c132f89c
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-14 17:02:59 -0300

    Adiciona novos desafios de código

---
+++ C#/Desafios de Código/numero-primo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VerificacaoNumeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            bool ehPrimo = VerificaPrimo(numero);
+
+            if (ehPrimo)
+            {
+                Console.WriteLine(numero + " é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine(numero + " não é um número primo.");
+            }
+        }
+
+        static bool VerificaPrimo(int numero)
+        {
+            if (numero <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i * i <= numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
