0c0d29737520dda76c26f9f236a11f9f2629d827
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-30 18:04:42 -0300

    Cria pasta 'String' com aplicação console MyApp para aprender como trabalhar com strings

---
+++ Cursos/Fundamentos do C# - Balta.io/Strings/MyApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+            id = new Guid("4bc7ba33-31f1-4dc7-89ef-a67d5e95bec4");
+            Console.WriteLine(id.ToString().Substring(0, 8));
+        }
+    }
+}
