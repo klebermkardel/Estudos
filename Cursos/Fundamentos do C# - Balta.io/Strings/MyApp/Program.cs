68b22a7e04655ee1b3c2735e9d51e3764443f317
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-30 18:30:18 -0300

    Cria conteúdo sobre interpolação de strings

--- Cursos/Fundamentos do C# - Balta.io/Strings/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Strings/MyApp/Program.cs
@@ -7,7 +7,8 @@ namespace MyApp
         static void Main(string[] args)
         {
             // Guids
-            Console.WriteLine("Guids");
+            Console.WriteLine("Guids:");
+            Console.WriteLine("");
 
             var id = Guid.NewGuid();
             id.ToString();
@@ -16,9 +17,34 @@ namespace MyApp
             
             Console.WriteLine("------------------");
 
+            // Interpolação de Strings
+            Console.WriteLine("Interpolação de Strings:");
+            Console.WriteLine("");
 
+            // 1ª forma
+            Console.WriteLine("Primeira forma:");
+            var price = 10.2;
+            var texto = "O preço do produto é " + price + " apenas na promoção";
 
-            
+            Console.WriteLine(texto);
+
+            Console.WriteLine("");
+
+            // 2ª forma
+            Console.WriteLine("Segunda forma:");
+            var texto2 = string.Format("O preço do produto é {0} apenas na promoção", price);
+
+            Console.WriteLine(texto2);
+
+            Console.WriteLine("");
+
+            // 3ª forma
+            Console.WriteLine("Segunda forma:");
+            var texto3 = $@"O preço
+            do produto é {price} apenas na promoção";
+
+            Console.WriteLine(texto3);
+
         }
     }
 }
