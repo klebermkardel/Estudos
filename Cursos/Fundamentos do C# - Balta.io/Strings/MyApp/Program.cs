936beaa79158a1586b6fa3241eae4313dcb16331
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-30 18:05:58 -0300

    Adiciona conteúdo sobre Guids

--- Cursos/Fundamentos do C# - Balta.io/Strings/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Strings/MyApp/Program.cs
@@ -6,6 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            // Guids
             var id = Guid.NewGuid();
             id.ToString();
             id = new Guid("4bc7ba33-31f1-4dc7-89ef-a67d5e95bec4");
