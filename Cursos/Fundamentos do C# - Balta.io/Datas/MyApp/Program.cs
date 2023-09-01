0ba21e7e379dd95daaec3ac5d87c75e87a055452
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-01 13:26:09 -0300

    Cria conteúdo sobre como obter valores específicos de uma data

--- Cursos/Fundamentos do C# - Balta.io/Datas/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Datas/MyApp/Program.cs
@@ -13,7 +13,6 @@ namespace MyApp
             Console.WriteLine($"{data} - valor padrão do DateTime"); // Retorna 01/01/0001 00:00:00
 
             Console.WriteLine("-----------------------");
-
             Console.WriteLine("");
 
             // Data Atual
@@ -24,7 +23,22 @@ namespace MyApp
             var data2 = DateTime.Now;
             Console.WriteLine($"{data2} - Retorna data e horário atual"); // Retorna data e horário atual"
 
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("");
 
+            // Obtendo valores da data
+            Console.WriteLine("Obtendo valores das datas:");
+            Console.WriteLine("");
+
+            var data3 = new DateTime(2023, 09, 01, 13, 14, 43);
+            Console.WriteLine($"{data3} - Retorna data pré definida (definir ano, mês, dia, hora, minutos, segundos)"); // Retorna data pré definida (definir ano, mês, dia, hora, minutos, segundos)
+            Console.WriteLine($"{data3.Year} - Retorna ano definido"); // Retorna ano definido
+            Console.WriteLine($"{data3.Month} - Retorna mês definido"); // Retorna mês definido
+            Console.WriteLine($"{data3.Day} - Retorna dia definido"); // Retorna dia definido
+            Console.WriteLine($"{data3.Hour} - Retorna hora definida"); // Retorna hora definida
+            Console.WriteLine($"{data3.Minute} - Retorna minutos definido"); // Retorna minutos definido
+            Console.WriteLine($"{data3.Second} - Retorna segundos definido"); // Retorna segundos definido
+            Console.WriteLine($"{data3.DayOfWeek} - Retorna dia da semana da data que foi defininida em inglês"); // Retorna dia da semana da data que foi defininida em inglês
         }
     }
 }
