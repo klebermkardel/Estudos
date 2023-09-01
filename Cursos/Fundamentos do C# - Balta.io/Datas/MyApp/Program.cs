0b7cacbad52fcab1e9dd5d3b81c39fd33cad2f22
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-01 13:10:28 -0300

    Cria conteúdo sobre DateTime e como obter a data e horário atual

--- Cursos/Fundamentos do C# - Balta.io/Datas/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Datas/MyApp/Program.cs
@@ -6,7 +6,25 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            
+            // DateTime - Struct Tipo Primitivo
+            var data = new DateTime(); // Composição dd/mm/aaaa hh:mm:ss
+            Console.WriteLine("DateTime:");
+            Console.WriteLine("");
+            Console.WriteLine($"{data} - valor padrão do DateTime"); // Retorna 01/01/0001 00:00:00
+
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("");
+
+            // Data Atual
+            Console.WriteLine("Data Atual:");
+
+            Console.WriteLine("");
+
+            var data2 = DateTime.Now;
+            Console.WriteLine($"{data2} - Retorna data e horário atual"); // Retorna data e horário atual"
+
+
         }
     }
 }
