2c105a704c659cdc82ba7d0336f9a1988f317144
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-03 14:11:32 -0300

    Adiciona conteúdo sobre a classe Math

--- Cursos/Fundamentos do C# - Balta.io/Moedas/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Moedas/MyApp/Program.cs
@@ -13,8 +13,8 @@ namespace MyApp
             Console.WriteLine("Melhor tipo (decimal):");
             Console.WriteLine("");
 
-            decimal valor = 10.25m;
-            Console.WriteLine($"{valor} - Retorna valor definido na variável '10.25'"); // Retorna valor definido na variável '10.25'
+            decimal valor = 10536.25m;
+            Console.WriteLine($"{valor} - Retorna valor definido na variável"); // Retorna valor definido na variável
 
             Console.WriteLine("----------------------------");
             Console.WriteLine("");
@@ -25,7 +25,18 @@ namespace MyApp
 
             Console.WriteLine($"{valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))} - Retorna valor definido formatado com o CultureInfo definido"); // etorna valor definido formatado com o CultureInfo definido
 
-            
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("");
+
+            // Math
+            Console.WriteLine("Math:");
+            Console.WriteLine("");
+
+            Console.WriteLine($"{Math.Round(valor)} - Round: Retorna valor arredondado pra baixo entre .01 e .50 e pra cima entre .51 e .99"); // Retorna valor arredondado pra baixo entre .01 e .50 e pra cima entre .51 e .99
+
+            Console.WriteLine($"{Math.Ceiling(valor)} -  Ceiling: Retorna valor arredondado para cima"); // Retorna valor arredondado para cima
+
+            Console.WriteLine($"{Math.Floor(valor)} - Floor: Retorna valor arredondado para baixo"); // Retorna valor arredondado para baixo
         }
     }
 }
