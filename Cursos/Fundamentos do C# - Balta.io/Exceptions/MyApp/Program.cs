3fd78822e1bc45621417fc96e42ea1f697d0e263
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-03 15:37:11 -0300

    Cria conteúdo sobre tratamentos try/catch

--- Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
@@ -7,14 +7,27 @@ namespace MyApp
         static void Main(string[] args)
         {
             // Execeptions - Tratamento de erros
+            // Toda vez que uma excessão ocorre, ela para a execução do nosso programa
 
             var arr = new int[3]{1, 2, 3};
 
-            for(var i = 0; i < 10; i++)
-            {
-                // System.IndexOutOfRangeException
-                Console.WriteLine(arr[i]);
+            // Try/Catch
+
+            try {
+                for(var i = 0; i < 10; i++)
+                {
+                    // System.IndexOutOfRangeException
+                    Console.WriteLine(arr[i]);
+                }
+            } catch(Exception ex) {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado!");
             }
+
+            
+
+            
         }
     }
 }
