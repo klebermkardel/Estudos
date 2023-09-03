bca26de22c2c70401af84b5f89351b032a7df3fd
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-03 15:58:39 -0300

    Adiciona conteúdo sobre Custom Excepetions

--- Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
@@ -31,6 +31,12 @@ namespace MyApp
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Falha ao cadastrar texto");
             }
+            catch(MinhaException ex) {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.QuandoAconteceu);
+                Console.WriteLine("Exceção customizada");
+            }
             catch(Exception ex) {
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
@@ -41,7 +47,16 @@ namespace MyApp
         static void Salvar(string texto)
         {
             if(string.IsNullOrEmpty(texto))
-                throw new ArgumentNullException("O texto não pode ser nulo ou vazio");  
+                throw new MinhaException(DateTime.Now);  
+        }
+
+        public class MinhaException : Exception
+        {
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+            public DateTime QuandoAconteceu { get; set; }
         }
     }
 }
