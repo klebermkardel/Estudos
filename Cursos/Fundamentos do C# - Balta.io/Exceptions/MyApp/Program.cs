6a54e4949e48dffa063c80a96f75f7924b82260c
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-03 15:50:14 -0300

    Cria conteúdo sobre como disparar erros (exceções)

--- Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
+++ Cursos/Fundamentos do C# - Balta.io/Exceptions/MyApp/Program.cs
@@ -14,25 +14,34 @@ namespace MyApp
             // Try/Catch (sempre tratar os erros do mais espefícico para o mais genérico)
 
             try {
-                for(var i = 0; i < 10; i++)
-                {
-                    // System.IndexOutOfRangeException
-                    Console.WriteLine(arr[i]);
-                }
+                // for(var i = 0; i < 10; i++)
+                // {
+                //     // System.IndexOutOfRangeException
+                //     Console.WriteLine(arr[i]);
+                // }
+
+                Salvar("");
             } catch(IndexOutOfRangeException ex) {
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Não encontrei o próximo índice na lista"); // vai rodar esse
+            }
+            catch(ArgumentNullException ex) {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Falha ao cadastrar texto");
             }
             catch(Exception ex) {
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Ops, algo deu errado!");
             }
+        }
 
-            
-
-            
+        static void Salvar(string texto)
+        {
+            if(string.IsNullOrEmpty(texto))
+                throw new ArgumentNullException("O texto não pode ser nulo ou vazio");  
         }
     }
 }
