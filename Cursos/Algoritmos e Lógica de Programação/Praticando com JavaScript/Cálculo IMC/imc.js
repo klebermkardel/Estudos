e138e41cad0d3d06c60e974d41eede6f1c1f804c
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-20 09:43:24 -0300

    Atualiza exercícios

--- Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Cálculo IMC/imc.js
+++ Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Cálculo IMC/imc.js
@@ -5,8 +5,18 @@ altura = parseFloat(prompt("Informe a sua altura: "))
 
 imc = peso / (Math.pow(altura, 2))
 
-if(imc >= 18.5 && imc < 25) {
-    alert(`IMC: ${imc.toFixed(2)}\n Parabéns, você está no seu peso ideal!`)
+if(imc < 17) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Muito abaixo do peso`)
+} else if (imc >= 17 && imc <= 18.5) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Abaixo do peso`)
+} else if (imc >= 18.5 && imc < 25) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Peso ideal`)
+} else if (imc >= 25 && imc < 30) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Sobrepeso`)
+} else if (imc >= 30 && imc < 35) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Obesidade`)
+} else if (imc >= 35 && imc < 40) {
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Obesidade Severa`)
 } else {
-    alert(`IMC: ${imc.toFixed(2)}\n Você não está na faixa de peso ideal!`)
+    alert(`IMC: ${imc.toFixed(2)}\n Situação: Obesidade Mórbida`)
 }
--- Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Média/media.js
+++ Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Média/media.js
@@ -5,10 +5,12 @@ n2 = parseFloat(prompt("Segunda nota: "))
 
 media = (n1+n2)/2
 
-if(media < 7) {
-    alert(`Média: ${media}\n Situação: Aluno Reprovado`)
-} else {
+if(media >= 7) {
     alert(`Média: ${media}\n Situação: Aluno Aprovado`)
+} else if(media >= 5 && media < 7) {
+    alert(`Média: ${media}\n Situação: Aluno em Recuperação`)
+} else {
+    alert(`Média: ${media}\n Situação: Aluno Reprovado`)
 }
 
 
