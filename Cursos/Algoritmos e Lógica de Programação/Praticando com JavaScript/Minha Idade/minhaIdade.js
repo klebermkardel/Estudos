d85556f262f54f4d5ebf4ee4592d4f39de18fc96
Kleber Molina Kardel <molinakardel@gmail.com>
2023-09-20 08:28:02 -0300

    Atualiza exercício com estrutura condicional composta

--- Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Minha Idade/minhaIdade.js
+++ Cursos/Algoritmos e Lógica de Programação/Praticando com JavaScript/Minha Idade/minhaIdade.js
@@ -4,4 +4,10 @@ ano_atual = parseInt(prompt("Em que ano nós estamos? "))
 ano_nasc = parseInt(prompt("Em que ano eu nasci? "))
 idade = ano_atual - ano_nasc
 
-alert(`Então eu tenho ${idade} anos!`)
+alert(`Em ${ano_atual} você terá ${idade} anos!`)
+
+if(idade >= 18) {
+    alert(`Você já é maior de idade!`)
+} else {
+    alert(`Você ainda não atingiu a maioridade!`)
+}
