868259f132fcbb12d2a6a5dda9d9b3db28168dc1
Kleber Molina Kardel <molinakardel@gmail.com>
2024-02-12 06:58:18 -0300

    Adiciona novo desafio

---
+++ 100 Days Of Code/Dia 2 - Greeting/index.js
@@ -0,0 +1,5 @@
+/* Dia 2: Faça um programa que solicite ao usuário seu nome e depois imprima uma saudação com esse nome. */
+
+let name = prompt("Olá! Qual o seu nome?")
+
+console.log(`É um prazer ter você por aqui, ${name}`);
