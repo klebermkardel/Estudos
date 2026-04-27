7379fcd9dae3acf177e84ceb4dc657433f08e5d3
klebermkardel <molinakardel@outlook.com.br>
2026-04-27 20:10:55 -0300

    Reorganização da pasta

--- Algoritmos e Lógica de Programação/3 -Estruturas de Repetição/4 - Controlando os Laços/Exemplos/break/script.js
+++
@@ -1,6 +0,0 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 7 === 0) {
-        console.log(`Primeiro número divisível por 7 encontrado: ${i}`);
-        break; // Sai do 'for' imprimindo apenas o primeiro número divisível por 7 encontrado
-    }
-}
--- Algoritmos e Lógica de Programação/3 -Estruturas de Repetição/4 - Controlando os Laços/Exemplos/continue/script.js
+++
@@ -1,7 +0,0 @@
-for (let i = 1; i <= 10; i++) {
-    // Se o número for par...
-    if (i % 2 === 0) {
-        continue; // ...pule o resto do código e vá para a próxima iteração (i++)
-    }
-    console.log(i);
-}
